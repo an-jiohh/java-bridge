fd84af12482d2336791b4c87d46eaffa0c97bfda
jiohh <dkswlgh99@naver.com>
2023-11-27 01:31:54 +0900

    feat(view): 입력 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,57 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import bridge.validation.InputValidator;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final InputValidator inputValidation;
+
+    public InputView() {
+        this.inputValidation = new InputValidator();
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String readLine = readLine();
+        inputValidation.validateBridgeSize(readLine);
+        return parserBridgeSize(readLine);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String readLine = readLine();
+        inputValidation.validateMoving(readLine);
+        return readLine;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String readLine = readLine();
+        inputValidation.validateGameCommand(readLine);
+        return readLine;
+    }
+
+    private int parserBridgeSize(String readLine){
+        int bridgeSize = 0;
+        try{
+            bridgeSize = Integer.parseInt(readLine);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 정수형 범위를 벗어났습니다.");
+        }
+        return bridgeSize;
     }
 }
---
+++ src/main/java/bridge/validation/InputValidator.java
@@ -0,0 +1,45 @@
+package bridge.validation;
+
+public class InputValidator {
+
+    public void validateBridgeSize(String readLine){
+        checkBlank(readLine);
+        checkDigit(readLine);
+    }
+
+    public void validateMoving(String readLine){
+        checkBlank(readLine);
+        checkMovingWord(readLine);
+    }
+
+    public void validateGameCommand(String readLine){
+        checkBlank(readLine);
+        checkGameCommandWord(readLine);
+    }
+
+    private void checkBlank(String readLine){
+        if(readLine.isBlank()){
+            throw new IllegalArgumentException("[ERROR] 빈 값이 입력되었습니다.");
+        }
+    }
+
+    private void checkDigit(String readLine){
+        for(int i = 0; i < readLine.length(); i++){
+            if(!Character.isDigit(readLine.charAt(i))){
+                throw new IllegalArgumentException("[ERROR] 숫자를 입력해야합니다.");
+            }
+        }
+    }
+
+    private void checkMovingWord(String readLine){
+        if(!readLine.equals("U") && !readLine.equals("D")){
+            throw new IllegalArgumentException("[ERROR] 이동하기 위해서는 U 또는 D를 입력해야합니다.");
+        }
+    }
+
+    private void checkGameCommandWord(String readLine){
+        if(!readLine.equals("R") && !readLine.equals("Q")) {
+            throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해야합니다.");
+        }
+    }
+}
